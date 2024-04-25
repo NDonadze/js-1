f750fc94b5c6d3efc5e9e3eccedff0b2511b299c
NDonadze <nikushadonadze20@gmail.com>
2024-04-25 16:09:34 +0400

    Finish everything

---
+++ script.js
@@ -0,0 +1,40 @@
+// work 1
+var userName="nika";
+var lastName="Donadze"
+let age=20;
+const hasHobbies=true;
+const gender="male"
+console.log(userName, lastName,gender, age, hasHobbies);
+
+// work 2
+let x=20;
+let y=40;
+let z=60;
+
+console.log(x+y+z);
+console.log(x-y-z);
+console.log((x+y)-z);
+console.log((x+y)/z);
+console.log(z*y);
+console.log(y/(z*y));
+console.log(x+y+z-(x*z)-(x*y));
+
+// work 3
+
+let university="smart academy";
+let courseName="Frontend & Seo Development"
+let location="tbilisi";
+let sibling="shorena";
+
+console.log("Hello!, I'm "+ userName + " " + lastName + " and i'm studying at "+university+" in "+courseName+" course."+" also, I live in "+location+" and i have one sibling called "+sibling+".");
+console.log(`Hello!, I'm ${userName} ${lastName} and i'm studying at ${university} in ${courseName} course. also, i live in ${location} and i have one sibling called ${sibling}`);
+
+// work 4
+
+let rectangle = {
+    x:7,
+    y:5,
+};
+
+console.log(`${rectangle.x*rectangle.y}`);
+
